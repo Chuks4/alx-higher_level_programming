eb322d8b3d6559021561e6b8a88beb07daff31ba
Agha Chukwuemeka <chukwuemekaagha733@gmail.com>
2022-11-30 17:39:57 +0100

    prints the last digit of a follwed followed by a string

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import random
+
+number = random.randint(-10000, 10000)
+str_1 = repr(number)
+str_2 = str_1[-1]
+int_last_num = int(str_2)
+
+if int_last_num > 5:
+    print(f"Last digit of {str_1} is {str_2} and is greater than 5")
+
+elif int_last_num == 0:
+    print(f"Last digit of {str_1} is {str_2} and is 0")
+
+else:
+    print(f"Last digit of {str_1} is {str_2} and is less than 6 and not 0")
