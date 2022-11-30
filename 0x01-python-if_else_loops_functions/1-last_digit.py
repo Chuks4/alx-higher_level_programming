f877f480571ce5d05dcb7912c4b927081f084159
Agha Chukwuemeka <chukwuemekaagha733@gmail.com>
2022-11-30 17:53:26 +0100

    prints the last digit of a follwed followed by a string

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,8 +10,8 @@ int_last_num = int(str_2)
 if int_last_num > 5:
     print(f"Last digit of {str_1} is {str_2} and is greater than 5")
 
-elif int_last_num == 0:
-    print(f"Last digit of {str_1} is {str_2} and is 0")
-
 elif int_last_num < 6 and int_last_num != 0:
     print(f"Last digit of {str_1} is {str_2} and is less than 6 and not 0")
+
+else:
+    print(f"Last digit of {str_1} is {str_2} and is {0})
