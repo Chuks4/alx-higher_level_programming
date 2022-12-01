5656850c37a95c22cdf6454e0cb7671a1cd1a006
Agha Chukwuemeka <chukwuemekaagha733@gmail.com>
2022-12-01 14:22:45 +0100

    A program that prints number from 0-99 separated by comma and a white space

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
-        print("0{:d}, ".format(i), end='')
-
-    elif i == 99:
-        print("{:d}".format(i))
+    if i == 99:
+        print("{:02d}".format(i))
 
     else:
-        print("{:d}, ".format(i), end='')
+        print("{:02d}".format(i), end=', ')
